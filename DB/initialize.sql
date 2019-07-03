4edfa081f5617365b9061bb78f1ae896d71ee4e9
zerossk <a.k.jack.ssk@gmail.com>
2019-07-03 13:29:39 +0900

    edit initialize.sql

--- DB/initialize.sql
+++ DB/initialize.sql
@@ -1,3 +1,13 @@
-CREATE TABLE NOT EXISTS img_data.tasks(
-	imgs 
-)
+CREATE USER `attendance_rec`@`%`;
+GRANT INSERT,SELECT,UPDATE,DELETE ON `attendance_rec_db`.* TO `attendance_rec`@`%`;
+
+CREATE DATABASE IF NOT EXISTS `attendance_rec_db`;
+
+CREATE TABLE IF NOT EXISTS `attendance_rec_db`.`user` (
+    `uuid`              CHAR(18)     NOT NULL,
+    `data`              DATE         NOT NULL,
+	`reason`            TEXT         NOT NULL,
+
+    PRIMARY KEY ( `uuid` )
+);
+
