000a427b3c99969d97729dc069b9620d0b296af8
zerossk <a.k.jack.ssk@gmail.com>
2019-07-09 14:36:18 +0900

    DB table test

--- DB_SERVER/initialize.sql
+++ DB_SERVER/initialize.sql
@@ -3,23 +3,24 @@ GRANT INSERT,SELECT,DELETE ON `api_data_db`.* TO `api`@`%`;
 
 CREATE DATABASE IF NOT EXISTS `api_data_db`;
 
-CREATE TABLE IF NOT EXISTS `api_data_db`.`imgs` (
+CREATE TABLE IF NOT EXISTS `api_data_db`.`twi_data` (
 	`id`		INT			NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	`img`		TEXT		NOT NULL,
+	`twi_id`	TEXT		NOT NULL,
+	`file_name`	TEXT		NOT NULL,
 	`add_date`	TIMESTAMP	NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 USE api_data_db;
 
-INSERT INTO imgs (img) VALUES
-('Twitter-test1'),
-('Twitter-test2'),
-('Twitter-test3'),
-('Twitter-test4'),
-('Twitter-test5'),
-('Twitter-test6'),
-('Twitter-test7'),
-('Twitter-test8'),
-('Twitter-test9'),
-('Twitter-test10');
+INSERT INTO twi_data (twi_id, file_name) VALUES
+('a','Twitter-test1'),
+('a','Twitter-test2'),
+('b','Twitter-test3'),
+('b','Twitter-test4'),
+('b','Twitter-test5'),
+('c','Twitter-test6'),
+('d','Twitter-test7'),
+('ee','Twitter-test8'),
+('ee','Twitter-test9'),
+('abc','Twitter-test10');
 
