5b19cd81af704fb33f9dfc29828fc084da9c449b
zerossk <a.k.jack.ssk@gmail.com>
2019-07-10 12:30:17 +0900

    API specification change

--- API_SERVER/server.go
+++ API_SERVER/server.go
@@ -10,16 +10,21 @@ import(
 	"github.com/ant0ine/go-json-rest/rest"
 )
 
-type postPageInput struct {
+type Page_Input struct {
 	Page json.Number
 }
 
-type postResultJSON struct {
+type TwiID_Page_Input struct {
+	Page json.Number
+	UserID string
+}
+
+type Result_JSON struct {
 	Result string
 }
 
 func postPage(Rw rest.ResponseWriter, req *rest.Request) {
-	input := postPageInput{}
+	input := Page_Input{}
 	err := req.DecodeJsonPayload(&input)
 	if err != nil {
 		rest.Error(Rw, err.Error(), http.StatusInternalServerError)
@@ -28,14 +33,14 @@ func postPage(Rw rest.ResponseWriter, req *rest.Request) {
 
 	log.Printf("input: %#v", input)
 
-	PNumber, err := input.Page.Int64()
+	PNum, err := input.Page.Int64()
 	if err != nil {
 		rest.Error(Rw, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	if PNumber != 0 {
-		Rw.WriteJson(&postResultJSON{
+	if PNum != 0 {
+		Rw.WriteJson(&Result_JSON{
 			"Page number is OK",
 		})
 		moduleDB.DB()
@@ -44,28 +49,46 @@ func postPage(Rw rest.ResponseWriter, req *rest.Request) {
 	}
 }
 
-func postUserID(Rw rest.ResponseWriter, req *rest.Request) {
-	var Tinput string
-	err := req.DecodeJsonPayload(&Tinput)
+func postTwiID_Page(Rw rest.ResponseWriter, req *rest.Request) {
+	input := TwiID_Page_Input{}
+	err := req.DecodeJsonPayload(&input)
 	if err != nil {
 		rest.Error(Rw, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	log.Printf("input: ", Tinput)
+	log.Printf("input: %#v", input)
+
+	PNum, _ := input.Page.Int64()
+
+	if input.UserID && input.Page == nil {
+		rest.Error(Rw, "UserID & Page number is required", 400)
+	} else if input.Page == nil {
+		Rw.WriteJson(&Result_JSON{
+			input.UserID,
+		})
+		moduleDB.DB()
+	} else if PNum != 0 {
+		Rw.WriteJson(&Result_JSON{
+			input.UserID+" "+input.Page,
+		})
+		moduleDB.DB()
+	} else {
+		rest.Error(Rw, "Page number is required", 400)
+	}
 }
 
 func main() {
 	api := rest.NewApi()
 	api.Use(rest.DefaultDevStack...)
 	router, err := rest.MakeRouter(
-		rest.Post("/page", postPage),
-		rest.Post("/userID", postUserID),
+		rest.Post("api/Page", postPage),
+		rest.Post("api/TwiID_Page", postTwiID_Page),
 	)
-
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	log.Printf("Server started.")
 
 	api.SetApp(router)
