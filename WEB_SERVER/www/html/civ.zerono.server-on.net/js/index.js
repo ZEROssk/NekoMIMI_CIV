b844e89e4e889f7c2c66d9e85a18476a66438909
ZEROssk <a.k.jack.ssk@gmail.com>
2019-10-24 14:41:08 +0900

    Add new function for web

--- WEB_SERVER/www/html/civ.zerono.server-on.net/js/index.js
+++ WEB_SERVER/www/html/civ.zerono.server-on.net/js/index.js
@@ -9,14 +9,11 @@ document.addEventListener("DOMContentLoaded", function() {
 	}
 	addThumbnailImg(path);
 
-	document.getElementById("search-btn").addEventListener("click", function(){
-		onSearchButtonClick();
-	});
 });
 
 function requestAjax(endpoint, callback) {
 	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function(){
+	xhr.onreadystatechange = function() {
 		if (this.readyState==4 && this.status==200) {
 			callback(this.response);
 		}
@@ -27,21 +24,30 @@ function requestAjax(endpoint, callback) {
 };
 
 function onSearchButtonClick() {
-	searchId = document.getElementById("input-keyword").value;
-	window.location.href = `/search?tid=${searchId}`;
+	if(window.event.keyCode == 13) {
+		searchId = document.getElementById("input-keyword").value;
+		window.location.href = `/search?tid=${searchId}`;
+	}
+}
+
+function originalImg() {
+	window.location.href = `/original?tid=${tId}&fname=${fName}`;
 }
 
 function addOriginalImg(url) {
+	let originalImg = document.getElementById('img-container');
+	originalImg.id = "original-img-container";
+
 	requestAjax(`http://civ.zerono.server-on.net:8888/api/v1/twimg${url}`, function(response){
 
 		let img = response.Image.FileName
 		let original =
-			'<div class="content-thumbnail" target="_blank">'+
-				`<img class="thumbnail-img" onclick="addOriginalImg()" src="../IMAGE/Twitter/${img}"/>`+
+			'<div class="content-original" target="_blank">'+
+				`<img class="original-img" src="../IMAGE/Twitter/${img}"/>`+
 			'</div>'
 		;
 
-		document.getElementById('img-container').insertAdjacentHTML('beforeend', original);
+		document.getElementById('original-img-container').insertAdjacentHTML('beforeend', original);
 	});
 }
 
@@ -50,9 +56,12 @@ function addThumbnailImg(url) {
 	
 		for(let i=0; i < response.Thumbnail.length; i++) {
 			let img = response.Thumbnail[i].FileName
+			let tid = response.Thumbnail[i].TwitterID
 			let thumbnail =
 				'<div class="content-thumbnail" target="_blank">'+
-					`<img class="thumbnail-img" onclick="addOriginalImg()" src="../IMAGE/Twitter/${img}"/>`+
+					`<a href="/original?tid=${tid}&fname=${img}">`+
+						`<img class="thumbnail-img" src="../IMAGE/Twitter/${img}"/>`+
+					'</a>'+
 				'</div>'
 			;
 
