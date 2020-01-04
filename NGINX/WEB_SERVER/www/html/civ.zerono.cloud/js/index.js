8287ef199bfaa31728b3bb617e38987ce89f6069
ZEROssk <a.k.jack.ssk@gmail.com>
2020-01-04 23:25:52 +0900

    upload page UI test

--- NGINX/WEB_SERVER/www/html/civ.zerono.cloud/js/index.js
+++ NGINX/WEB_SERVER/www/html/civ.zerono.cloud/js/index.js
@@ -179,11 +179,64 @@ function addUpload() {
 		`<form enctype="multipart/form-data" action="${api}/upload" method="post">`+
 			'<input type="file" name="uploadIMG" multiple="multiple" accept="image/*">'+
 			'<input type="submit" value="Upload">'+
-		'</form>'
+		'</form>'+
+		'<div id="drop_zone">ドロップ</div><div id="preview"></div><button type="button" id="post">post</button>'
 	;
 
 	home.insertAdjacentHTML('beforeend', homeContent);
 }
+
+var formData = new FormData();
+var dropZone = document.getElementById("drop_zone");
+
+dropZone.addEventListener("dragover", function(e) {
+	  e.stopPropagation();
+	  e.preventDefault();
+
+	  this.style.background = "#ff3399";
+}, false);
+
+dropZone.addEventListener("dragleave", function(e) {
+	  e.stopPropagation();
+	  e.preventDefault();
+
+	  this.style.background = "#ffffff";
+}, false);
+
+dropZone.addEventListener("drop", function(e) {
+	e.stopPropagation();
+	e.preventDefault();
+
+	this.style.background = "#ffffff";
+
+	var files = e.dataTransfer.files;
+	for (var i = 0; i < files.length; i++) {
+		(function() {
+			var fr = new FileReader();
+			fr.onload = function() {
+				var div = document.createElement('div');
+
+				var img = document.createElement('img');
+				img.setAttribute('src', fr.result);
+				div.appendChild(img);
+
+				var preview = document.getElementById("preview");
+				preview.appendChild(div);
+			};
+			fr.readAsDataURL(files[i]);
+		})();
+
+		formData.append("file", files[i]);
+	}
+}, false);
+
+var postButton = document.getElementById("post");
+postButton.addEventListener("click", function() {
+	  var request = new XMLHttpRequest();
+	  request.open("POST", `${api}/upload`);
+	  request.send(formData);
+});
+
 
 function addOriginalImg(v) {
 	let orImg = document.getElementById('img-container');
