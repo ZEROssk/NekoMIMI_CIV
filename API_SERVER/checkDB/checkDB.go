8dd8f7aa7653753c0728ba62075bfbdd56ca3578
ZEROssk <a.k.jack.ssk@gmail.com>
2021-12-10 21:55:04 +0900

    Add File Type Check

--- API_SERVER/checkDB/checkDB.go
+++ API_SERVER/checkDB/checkDB.go
@@ -2,10 +2,12 @@ package checkDB
 
 import(
 	"os"
+	"io/ioutil"
 	"log"
 	"time"
 	"strings"
-	"io/ioutil"
+	"bytes"
+	"net/http"
 	"image"
 
 	"main/useDB"
@@ -18,11 +20,23 @@ func readDir(p string) []os.FileInfo {
 }
 
 func newIMG(files []os.FileInfo, path string) {
-	for _, f := range files {
+	for i, f := range files {
 		FB, _ := os.Open(path+"/"+f.Name())
 		defer FB.Close()
 
-		decImg, format, err := image.Decode(FB)
+		bufData, err := ioutil.ReadAll(FB)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		buf := bytes.NewBuffer(bufData)
+		mimeType := http.DetectContentType(buf.Bytes())
+		if mimeType != "image/jpeg" && mimeType != "image/png" {
+			continue
+		}
+
+		decImg, format, err := image.Decode(buf)
 		if err != nil {
 			log.Println(err)
 			return
@@ -31,6 +45,8 @@ func newIMG(files []os.FileInfo, path string) {
 			saveIMG.SaveThumbnail(decImg, f.Name(), format)
 			useDB.DBaddImg(ID, f.Name())
 		}
+
+		log.Println("Progress: ", i+1, "/", len(files))
 	}
 }
 
@@ -39,8 +55,10 @@ func CheckDB(path string) {
 
 	r := useDB.DBcheckData()
 	if r == 0 {
+		log.Printf("Add New Image")
 		newIMG(readDir(path), path)
 	} else if r != len(readDir(path)) {
+		log.Printf("Update New Image")
 		StStamp := useDB.DBcheckCreatedAt()
 		rTStamp, _ := time.Parse(tformat, StStamp)
 
